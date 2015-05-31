0fe8360f6fc49778f0e0c2cae60a3103f58dc4c1
Sam Kovaka <skovaka@clarku.edu>
2015-05-31 16:37:22 -0400

    Added basic functionality for all the pages

---
+++ grader/forms.py
@@ -0,0 +1,47 @@
+from django.forms import ModelForm, Form, FileField, ChoiceField
+from grader.models import Assignment, Grade
+
+class AssgnForm(ModelForm):
+    """
+    Form for creating a new assignment
+    """
+    class Meta:
+        model = Assignment
+        fields = ['title', 'code', 'desc', 'due_date', 'max_grade', 'weight', 'max_subs']
+
+class SubmitForm(Form):
+    """
+    Form for submitting an assignment
+    """
+    
+    sub_file = FileField(allow_empty_file=False)
+    
+    student = ChoiceField()#This is temporary for testing - should be based on login
+    
+    def __init__(self, assignment, *args, **kw):
+        """
+        Associate the form with an assignment
+        """
+        
+        super(Form, self).__init__(*args, **kw)
+        
+        self.fields['student'].choices = map(lambda s: (s.id, "%s %s" % (s.first_name, s.last_name)), assignment.course.students.all())
+        self.assignment = assignment
+
+class GradeForm(ModelForm):
+    class Meta:
+        model = Grade
+        fields = ['grader', 'grade', 'comments']
+        
+    def __init__(self, sub, *args, **kw):
+        """
+        Associate the form with a submission
+        Grader field is temporary for testing
+        """
+        
+        super(ModelForm, self).__init__(*args, **kw)
+        
+        self.fields['grader'].choices = map(lambda u: (u.id, "%s %s" % (u.first_name, u.last_name)), sub.assignment.course.instructors.all())
+        self.fields['grade'].max_value = sub.assignment.max_grade
+        
+    
