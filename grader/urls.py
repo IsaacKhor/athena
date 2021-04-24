eaaf045b9ccdf77f0effda3ab8fab49545d040e0
Isaac Khor <s.github@isaackhor.com>
2021-04-24 10:40:17 -0400

    Rework urls to be more REST-like

--- grader/urls.py
+++ grader/urls.py
@@ -5,25 +5,28 @@ from grader import views, file_access
 
 app_name = 'grader'
 urlpatterns = [
-    path(''                                                         , views.home       ,               name='home'),
-    path('course/<int:courseid>/add_assgn'                          , views.course,                    name='add_assgn'),
-    path('course/<int:courseid>'                                    , views.course,                    name='course'),
-    path('add_assgn/<int:courseid>'                                 , views.edit_assgn,                name='add_assgn'),
-    path('edit_assgn/<int:courseid>/<int:assgnid>'                  , views.edit_assgn,                name='edit_assgn'),
-    path('add_course/'                                              , views.edit_course,               name='add_course'),
-    path('edit_course/<int:courseid>'                               , views.edit_course,               name='edit_course'),
-    path('assignment/<int:assgnid>'                                 , views.assignment,                name='assignment'),
-    path('submissions/<int:assgnid>/<int:userid>'                   , views.submissions,               name='submissions'),
+    path('course/<int:courseid>/edit'                               , views.edit_course,               name='edit_course'),
+    path('course/<int:courseid>/add_assgn'                          , views.edit_assgn,                name='add_assgn'),
+    path('course/<int:courseid>/download_grades'                    , file_access.grades_download,     name='grades_download'),
+    path('course/<int:courseid>/<str:filename>/delete'              , file_access.course_file_delete,  name='course_file_delete'),
     path('course/<int:courseid>/<str:filename>'                     , file_access.course_file_download,name='course_file_download'),
-    path('course/<int:courseid>/delete_file/<str:filename>'         , file_access.course_file_delete,  name='course_file_delete'),
+    path('course/<int:courseid>'                                    , views.course,                    name='course'),
+
+    path('edit_assgn/<int:courseid>/<int:assgnid>'                  , views.edit_assgn,                name='edit_assgn'),
+
+    path('assignment/<int:assgnid>/submissions/<int:userid>'        , views.submissions,               name='submissions'),
+    path('assignment/<int:assgnid>/<str:filename>/delete'           , file_access.assgn_file_delete,   name='assgn_file_delete'),
     path('assignment/<int:assgnid>/<str:filename>'                  , file_access.assgn_file_download, name='assgn_file_download'),
-    path('assignment/<int:assgnid>/delete_file/<str:filename>'      , file_access.assgn_file_delete,   name='assgn_file_delete'),
-    path('download_grades/<int:courseid>'                           , file_access.grades_download,     name='grades_download'),
-    path('download_subfile/<int:subid>'                             , file_access.submission_download, name='submission_download'),
-    path('download_subfile/<int:subid>/<str:subdir>/<str:filename>' , file_access.submission_download, name='submission_download_subdir'),
-    path('delete_subfile/<int:subid>/<str:subdir>/<str:filename>'   , file_access.submission_delete,   name='submission_delete_subdir'),
-    path('remove_grade/<int:gradeid>'                               , file_access.remove_grade,        name='remove_grade'),
-    path('reset_autograde/<int:gradeid>'                            , file_access.reset_autograde,     name='reset_autograde'),
+    path('assignment/<int:assgnid>'                                 , views.assignment,                name='assignment'),
+
+    path('submissions/<int:subid>/download'                         , file_access.submission_download, name='submission_download'),
+    path('submissions/<int:subid>/<str:subdir>/<str:filename>/download', file_access.submission_download, name='submission_download_subdir'),
+    path('submissions/<int:subid>/<str:subdir>/<str:filename>/delete', file_access.submission_download, name='submission_download_subdir'),
+
+    path('grades/<int:gradeid>/remove'                              , file_access.remove_grade,        name='remove_grade'),
+    path('grades/<int:gradeid>/reset_autograde'                     , file_access.reset_autograde,     name='reset_autograde'),
+
     path('login/'                                                   , views.login,                     name='login'),
     path('logout/'                                                  , views.logout,                    name='logout'),
+    path(''                                                         , views.home       ,               name='home'),
 ]
